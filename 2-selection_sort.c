5a0750b3df96565325ad81bbe4d4a7dada08c85d
mosesgitonga <infosec947@gmail.com>
2023-06-26 02:57:31 -0700

    selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,29 @@
-#include "main.h"
-
+#include "sort.h"
+/**
+ * selection_sort - selection sort ALGO
+ * @array: array to be sorted
+ * @size: size of the array
+ */
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j, min, tmp;
 
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (i != min)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+	}
 }
